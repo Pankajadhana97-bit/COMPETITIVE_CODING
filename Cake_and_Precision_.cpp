47845ad0fc6bb01da34c2a910290fe31e1e277f7
Pankajadhana97-bit <geekyblindcoder@gmail.com>
2020-11-04 07:45:56 +0530

    wrong answer

---
+++ Cake_and_Precision_.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+/*
+Pankaj Adhana
+Panjab university;
+*/
+
+/* Defined values---------------------------------------------------- */
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define cases  int t=1;cin>>t; while(t--) { solve();} return 0
+/*--------------------------------------------------------------------*/
+using namespace std;
+void solve()
+{
+   long long int k;
+  cin>>k;
+  cout<<k+1<<endl;
+}
+int main() 
+{ 
+    fast_io;
+    cases;
+} 
