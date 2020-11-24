c5f2a2535385d1c34b7c76972672a298c4e0d383
Pankajadhana97-bit <geekyblindcoder@gmail.com>
2020-11-24 23:40:39 +0530

    codechef

--- Connected_Components_in_a_Graph.cpp
+++ Connected_Components_in_a_Graph.cpp
@@ -10,24 +10,42 @@ Panjab university;
 #define ll int64_t
 /*--------------------------------------------------------------------*/
 using namespace std;
+vector<int> ar[100001];
+int vis[100001];
+ 
+void dfs(int node)
+{
+	vis[node] = 1;
+ 
+	for(int child : ar[node])
+	  if(!vis[child])
+	    dfs(child);
+ 
+}
 void solve()
 {
-  int queries;
-  cin>>queries;
-  while(queries--)
-  {
-      string str;
-      cin>>str;
-    for(int i=0;i<20;i++)
-    {
-       
-     
-        
-    }
-  }
+     int n,m;
+	cin>>n>>m;
+ 
+	for(int i=1;i<=m;i++)
+	{
+	int	a,b;
+	cin>>a>>b ;
+	 ar[a].push_back(b);
+	 ar[b].push_back(a);
+	}
+ 
+	int cc = 0;
+ 
+	for(int i=1;i<=n;i++)
+	if(!vis[i])
+	dfs(i) , cc++;
+ 
+	cout<<cc;
 }
 int main() 
 { 
     fast_io;
-    cases;
+   // cases;
+    solve();
 } 
