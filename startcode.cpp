059a09418e7557557ef899ef00ea1a3ced294937
Pankajadhana97-bit <geekyblindcoder@gmail.com>
2020-11-26 14:09:39 +0530

    codechef

--- startcode.cpp
+++ startcode.cpp
@@ -12,12 +12,12 @@ Panjab university;
 using namespace std;
 void solve()
 {
- int n=1000;
+ int n=10;
  for(int i=0;i<n;i++)
  {
- 	for(int j=0;j<i;j++)
- 		cout<<"*";
- 	    cout<<endl;
+for(int j=0;j<i;j++)
+cout<<"*";
+cout<<endl;
  }
 }
 int main() 
