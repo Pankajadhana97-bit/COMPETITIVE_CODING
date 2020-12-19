88155eb7009dee256e482f20fafb2f2c1d5c7990
Pankajadhana97-bit <geekyblindcoder@gmail.com>
2020-12-19 10:40:21 +0530

    codechef

--- Find_Minimum.cpp
+++ Find_Minimum.cpp
@@ -3,11 +3,10 @@
 Pankaj Adhana
 Panjab university;
 */
-
 /* Defined values----------------------------------------------------  */
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
 #define cases  int t=1;cin>>t; while(t--) { solve();} return 0
-#define ll int64_t
+#define ll long long int
 #define pi 3.14159265358993
 #define mod (int)1e9
 #define mod7 (int)1e7
@@ -15,33 +14,26 @@ Panjab university;
 using namespace std;
 void solve()
 {
- string str;
- cin>>str;
- int k;
- cin>>k;
- if(k==0)
- cout<<str<<endl;
- else
- {
-     for(int i=0;i<str.length();i++)
-     {
-         if(k>0)
-         {
-             cout<<9;
-             k--;
-         }
-         else
-         {
-             cout<<str[i];
-         }
-     }
-     cout<<endl;
- }
- 
+ll n;
+cin>>n;
+ll div;
+ll sum;
+ll new_sum=LONG_LONG_MAX;
+
+ll i;
+for(i=sqrt(n);i>0;i--)
+{
+    if(n%i==0)
+    {
+     div=n/i;
+     sum=div+i;
+    }
+    new_sum=min(new_sum,sum);   
 }
-int main() 
-{ 
-    fast_io;
-    //cases;
-    solve();
-} 
+ cout<<new_sum<<endl;
+}
+int main()
+{
+fast_io;
+cases;
+}
